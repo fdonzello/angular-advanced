38dee5666a49e1832dead054654de28ec13091c2
Francesco Donzello <francesco.donzello@gmail.com>
2023-03-08 17:50:54 +0100

    added bank account module

---
+++ src/app/bank-account/bank-account.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BankAccountRoutingModule } from './bank-account-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BankAccountRoutingModule
+  ]
+})
+export class BankAccountModule { }
